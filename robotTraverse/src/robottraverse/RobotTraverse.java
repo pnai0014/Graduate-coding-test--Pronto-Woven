f319284ee2ef6279b781a4f750020da33e820fba
pnai0014 <54134673+pnai0014@users.noreply.github.com>
2021-02-20 16:51:54 +1100

    Taking input from the user and validating using regular expressions.

--- robotTraverse/src/robottraverse/RobotTraverse.java
+++ robotTraverse/src/robottraverse/RobotTraverse.java
@@ -9,13 +9,27 @@ package robottraverse;
  *
  * @author naikp
  */
+import java.util.*;
+import java.util.regex.*;
+
 public class RobotTraverse {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Node currentPosition = new Node();
+        String input = "";
+        Matcher m;
+        String regex = "^([FBLR]{1}[1-9]\\d*)(,[FBLR]{1}[1-9]\\d*)*$";
+        Pattern p = Pattern.compile(regex);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Please enter the input String");
+            input = sc.next();
+            m = p.matcher(input);
+        } while (!m.matches());
+
+           
     }
-    
 }
